1ffb3b056286a34efbc2a6a344cc30682fc5e635
Natasa Nikolic <36802892+RofiProfi@users.noreply.github.com>
2020-11-23 19:14:01 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -8,6 +8,9 @@ class DB{
         $settings['dbhost'] = '127.0.0.1';
         $settings['dbuser'] = 'root';
         $settings['dbpass'] = '';
+	try{
+            $dsn = 'mysql:dbname=' . $settings['dbname'] . ';host=' . $settings['dbhost'];
+            $pdo = new PDO($dsn, $settings['dbuser'], $settings['dbpass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 */
         $host="ec2-54-247-122-209.eu-west-1.compute.amazonaws.com";
         $user="vfvwpqwscrjmpt";
@@ -18,10 +21,6 @@ class DB{
 			$dsn="pgsql:host=".$host.";port=".$port.";dbname=".$dbname.";user=".$user.";password=".$password.";";
          $pdo = new PDO($dsn,$user,$password);
         
-        try{
-            $dsn = 'mysql:dbname=' . $settings['dbname'] . ';host=' . $settings['dbhost'];
-            $pdo = new PDO($dsn, $settings['dbuser'], $settings['dbpass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
