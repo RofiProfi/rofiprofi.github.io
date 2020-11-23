8bbc83fe56d961dce45cf7ef62db8547a1e8e57a
Natasa Nikolic <36802892+RofiProfi@users.noreply.github.com>
2020-11-23 19:05:37 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,12 +3,21 @@ class DB{
     private static $factory;
 
     public static function createInstance($config = null)
-    {
+    {/* //konekcija za mysql preko localhost-a
         $settings['dbname'] = 'kozmolend';
         $settings['dbhost'] = '127.0.0.1';
         $settings['dbuser'] = 'root';
         $settings['dbpass'] = '';
-
+*/
+        $host="ec2-54-247-122-209.eu-west-1.compute.amazonaws.com";
+        $user="vfvwpqwscrjmpt";
+        $password="346c9358cc42f99542d08c0eb971385cddd0ea15184cefe4fcf703ff99612a13";
+        $dbname="dfv2eet43gqv6n";
+        $port="5432";
+        try{
+			$dsn="pgsql:host=".$host.";port=".$port.";dbname=".$dbname.";user=".$user.";password=".$password.";";
+         $pdo = new PDO($dsn,$user,$password);
+        
         try{
             $dsn = 'mysql:dbname=' . $settings['dbname'] . ';host=' . $settings['dbhost'];
             $pdo = new PDO($dsn, $settings['dbuser'], $settings['dbpass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
