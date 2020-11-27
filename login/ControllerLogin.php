187ebdb98b81ba734531ce52c3fe6a551fb4a0d6
Natasa Nikolic <36802892+RofiProfi@users.noreply.github.com>
2020-11-27 04:26:53 +0100

    Add files via upload

--- login/ControllerLogin.php
+++ login/ControllerLogin.php
@@ -2,119 +2,131 @@
 
 require_once '../login/DAOLogin.php';
 
-class ControllerLogin{
+class ControllerLogin
+{
 
-    public function pocetna() {       
-        header("Location:../korisnik/?action=pocetna");
-    }
-    
-    public function gologin() {
-       include 'login.php';
-        
-    }
-    
-    public function gocart() {
-        include '../kolica/cart.php';
-    }
-    
-    function goRegister() {
-        include '../login/login.php';
-    }
-    
-    function goRacun(){
-        include '../racun/racun.php';
-    }
-  
-    function logout(){
-        session_start();
-        session_unset();
-        session_destroy();
-        header("Location:../korisnik/?action=pocetna");
-    }
-    
-    function testInput ($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-        
-	 function login(){
-	    $controller=new ControllerLogin();
-	    $customer_email = isset($_POST['customer_email'])? $controller->testInput($_POST['customer_email']): "";
-	    $customer_pass = isset($_POST['customer_pass'])? $controller->testInput($_POST['customer_pass']): "";
-	    
-	    if(!empty($customer_email) && !empty($customer_pass)){
-	        $dao = new DAOLogin();
-	        $klijent = $dao->selectKlijentByUsernameAndPassword($customer_email,$customer_pass);
-	        
-	        if ($klijent){
-	            // LogIN OK
-	            // dodavanje ulogovanog u sesiju
-	            session_start();
-	            $_SESSION['ulogovan'] = $klijent;
-	            
-	            
-	            //include 'pocetna.php';
-	            header("Location:../korisnik/");
-	        }else{
-	            // LogIN ERROR
-	            $msg = 'Pogresni email ili sifra!';
-	            include 'login.php';
-	        }
-	    }else{
-	        $msg = 'Morate popuniti sva polja!';
-	        include 'login.php';
-	    }
+	public function pocetna()
+	{
+		header("Location:../korisnik/?action=pocetna");
 	}
 
-	
-	
-	function register() {
-	    $controller=new ControllerLogin();
-	    
-	    $customer_name = isset($_POST['customer_name'])? $controller->testInput($_POST['customer_name']): "";
-	    
-	    $customer_email = isset($_POST['customer_email'])? $controller->testInput($_POST['customer_email']): "";
-	    $customer_pass = isset($_POST['customer_pass'])? $controller->testInput($_POST['customer_pass']): "";
-	    $password_c = isset($_POST['password_c'])? $controller->testInput($_POST['password_c']): "";
-	    $customer_lastname = isset($_POST['customer_lastname'])? $controller->testInput($_POST['customer_lastname']): "";
-	    if(!empty($customer_name) && !empty($customer_lastname) && !empty($customer_email) && !empty($customer_pass) && !empty($password_c)) {
-	        if (strlen($customer_pass) >= 8) {
-	            if($customer_pass == $password_c) {
-	                $daoC = new DAOLogin();
-	                $userC = $daoC->klijentExistByUsername($customer_email); //proverava da li postoji email vec u bazi
-	                if( $userC>0) {
-	                     
-	                    $msg1 = "Username postoji, izaberite drugi";
-	                    include 'login.php';
-	                } else{$dao = new DAOLogin();
-	                //$password = md5($password); //Calculate the md5 hash of a string
-	                $newuser = $dao->insertUser($customer_name,$customer_lastname, $customer_email,$customer_pass);
-	                
-	                
-	                $klijent = $dao->selectKlijentByUsernameAndPassword($customer_email,$customer_pass);
-	                if ($klijent)
-	                    session_start();
-	                    $_SESSION['ulogovan'] = $klijent;
-	                    $msg1 = "Uspesno ste se registrovali";
-	                    header("Location:../korisnik/");
-	                    
-	                }} else {
-	                $msg1 = "Sifra i potvrdjena sifra moraju biti iste";
-	                include 'login.php';
-	            }
-	        } else {
-	            $msg1 = "Sifra mora imati 8 i vise karaktera";
-	            include 'login.php';
-	        }
-	    } else {
-	        $msg1 = "Morate popuniti sva polja";
-	        include 'login.php';
-	    }
+	public function gologin()
+	{
+		include 'login.php';
 	}
-    
+
+	public function gocart()
+	{
+		include '../kolica/cart.php';
+	}
+
+	function goRegister()
+	{
+		include '../login/login.php';
+	}
+
+	function goRacun()
+	{
+		include '../racun/racun.php';
+	}
+
+	function logout()
+	{
+		session_start();
+		session_unset();
+		session_destroy();
+		header("Location:../korisnik/?action=pocetna");
+	}
+
+	function testInput($data)
+	{
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	function login()
+	{
+		$controller = new ControllerLogin();
+		$customer_email = isset($_POST['customer_email']) ? $controller->testInput($_POST['customer_email']) : "";
+		$customer_pass = isset($_POST['customer_pass']) ? $controller->testInput($_POST['customer_pass']) : "";
+
+		if (!empty($customer_email) && !empty($customer_pass)) {
+			$dao = new DAOLogin();
+			$klijent = $dao->selectKlijentByUsernameAndPassword($customer_email, $customer_pass);
+
+			if ($klijent) {
+				// LogIN OK
+				// dodavanje ulogovanog u sesiju
+				session_start();
+				$_SESSION['ulogovan'] = $klijent;
+
+
+				//include 'pocetna.php';
+				header("Location:../korisnik/");
+			} else {
+				// LogIN ERROR
+				$msg = 'Pogresni email ili sifra!';
+				include 'login.php';
+			}
+		} else {
+			$msg = 'Morate popuniti sva polja!';
+			include 'login.php';
+		}
+	}
+
+
+
+	function register()
+	{
+		$controller = new ControllerLogin();
+		$daoC = new DAOLogin();
+		$customer_id = print_r(array_values($daoC->selectUserId()));
+		var_dump($customer_id);
+		if ($customer_id != 0 && $customer_id != null) {
+			$customer_id++;
+		} else {
+			$customer_id = 0;
+		}
+		$customer_name = isset($_POST['customer_name']) ? $controller->testInput($_POST['customer_name']) : "";
+		$customer_email = isset($_POST['customer_email']) ? $controller->testInput($_POST['customer_email']) : "";
+		$customer_pass = isset($_POST['customer_pass']) ? $controller->testInput($_POST['customer_pass']) : "";
+		$password_c = isset($_POST['password_c']) ? $controller->testInput($_POST['password_c']) : "";
+		$customer_lastname = isset($_POST['customer_lastname']) ? $controller->testInput($_POST['customer_lastname']) : "";
+		if (!empty($customer_name) && !empty($customer_lastname) && !empty($customer_email) && !empty($customer_pass) && !empty($password_c)) {
+			if (strlen($customer_pass) >= 8) {
+				if ($customer_pass == $password_c) {
+					$daoC = new DAOLogin();
+					$userC = $daoC->klijentExistByUsername($customer_email); //proverava da li postoji email vec u bazi
+					if ($userC > 0) {
+
+						$msg1 = "Username postoji, izaberite drugi";
+						include 'login.php';
+					} else {
+						$dao = new DAOLogin();
+						//$password = md5($password); //Calculate the md5 hash of a string
+						$newuser = $dao->insertUser($customer_id, $customer_name, $customer_lastname, $customer_email, $customer_pass);
+
+
+						$klijent = $dao->selectKlijentByUsernameAndPassword($customer_email, $customer_pass);
+						if ($klijent)
+							session_start();
+						$_SESSION['ulogovan'] = $klijent;
+						$msg1 = "Uspesno ste se registrovali";
+						header("Location:../korisnik/");
+					}
+				} else {
+					$msg1 = "Sifra i potvrdjena sifra moraju biti iste";
+					include 'login.php';
+				}
+			} else {
+				$msg1 = "Sifra mora imati 8 i vise karaktera";
+				include 'login.php';
+			}
+		} else {
+			$msg1 = "Morate popuniti sva polja";
+			include 'login.php';
+		}
+	}
 }
-	
-	
-?>
--- login/DAOLogin.php
+++ login/DAOLogin.php
@@ -1,75 +1,71 @@
 <?php
 require_once '../config/db.php';
 
-class DAOLogin {
+class DAOLogin
+{
 	private $db;
 
-	// osoba
 	private $KLIJENTEXISTBYUSERNAME = "SELECT count(*) FROM korisnici WHERE korisnik_email = ?";
 	private $SELECTKLIJENTBYUSERNAMEANDPASSWORD = "SELECT * FROM korisnici WHERE korisnik_email = ? AND korisnik_sifra = ?";
-	
-	//artika
-	
-	private $INSERTUSER = "INSERT INTO korisnici (korisnik_ime, korisnik_prezime, korisnik_email, korisnik_sifra) VALUES (?,?, ?, ?)";
+	private $INSERTUSER = "INSERT INTO korisnici (korisnik_id,korisnik_ime, korisnik_prezime, korisnik_email, korisnik_sifra) VALUES (?,?,?, ?, ?)";
+	private $SELECTUSERID = "SELECT korisnik_id FROM korisnici ORDER BY korisnik_id DESC LIMIT 1";
 
-
-	
 	public function __construct()
 	{
 		$this->db = DB::createInstance();
 	}
 
-	public function insertUser($korisnik_ime,$korisnik_prezime,$korisnik_email, $korisnik_sifra)
+	public function insertUser($korisnik_id, $korisnik_ime, $korisnik_prezime, $korisnik_email, $korisnik_sifra)
 	{
-	    $statement = $this->db->prepare($this->INSERTUSER);
-	    $statement->bindValue(1, $korisnik_ime);
-	    $statement->bindValue(2, $korisnik_prezime);
-	    
-	    $statement->bindValue(3, $korisnik_email);
-	    $statement->bindValue(4, $korisnik_sifra);
-		
+		$statement = $this->db->prepare($this->INSERTUSER);
+		$statement->bindValue(1, $korisnik_id);
+		$statement->bindValue(2, $korisnik_ime);
+		$statement->bindValue(3, $korisnik_prezime);
+		$statement->bindValue(4, $korisnik_email);
+		$statement->bindValue(5, $korisnik_sifra);
 		$statement->execute();
 	}
-	
-	
+
+	public function selectUserId()
+	{
+		$statement = $this->db->prepare($this->SELECTUSERID);
+		$statement->execute();
+		$result = $statement->fetch();
+		return $result;
+	}
 
 	public function selectKlijentById($idosoba)
 	{
 		$statement = $this->db->prepare($this->SELECTARTIKALBYID);
 		$statement->bindValue(1, $idosoba);
-		
 		$statement->execute();
-		
 		$result = $statement->fetch();
 		return $result;
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	public function klijentExistByUsername($korisnik_email)
 	{
 		$statement = $this->db->prepare($this->KLIJENTEXISTBYUSERNAME);
 		$statement->bindValue(1, $korisnik_email);
-		
+
 		$statement->execute();
-		
+
 		return  $statement->fetchColumn();
 	}
-	
+
 	public function selectKlijentByUsernameAndPassword($korisnik_email, $korisnik_sifra)
 	{
 		$statement = $this->db->prepare($this->SELECTKLIJENTBYUSERNAMEANDPASSWORD);
 		$statement->bindValue(1, $korisnik_email);
 		$statement->bindValue(2, $korisnik_sifra);
-		
+
 		$statement->execute();
-		
+
 		$result = $statement->fetch();
 		return $result;
 	}
-	
-	
 }
-?>
